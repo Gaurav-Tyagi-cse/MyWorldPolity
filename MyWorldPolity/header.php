767586afc42befbbb15c9f234e9a30076e84df4f
Gaurav Tyagi <118004334+Gaurav-Tyagi-cse@users.noreply.github.com>
2024-10-05 12:32:28 +0530

    first commit

---
+++ MyWorldPolity/header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+<head>
+	<title>World_Polity</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Unica University Template">
+	<meta name="keywords" content="event, unica, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/themify-icons.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- header section -->
+	<header class="header-section">
+		<div class="container">
+			<!-- logo -->
+			<a href="index.html" class="site-logo"><img src="img/mainlogo.jpg" height="35px" width="140px" alt=""></a>
+			<div class="nav-switch">
+				<i class="fa fa-bars"></i>
+			</div>
+			<div class="header-info">
+				<div class="hf-item">
+					<i class="fa fa-clock-o"></i>
+					<p><span>Working time:</span>Monday - Saturday: 08 AM - 06 PM</p>
+				</div>
+				<div class="hf-item">
+					<i class="fa fa-map-marker"></i>
+					<p><span>Find us:</span>Sani Mandir Road, Haibargaon, Nagaon, Assam,782002</p>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- header section end-->
+
+
+	<!-- Header section  -->
+	<nav class="nav-section">
+		<div class="container">
+			<div class="nav-right">
+				<a href=""><i class="fa fa-search"></i></a>
+				<a href=""><i class="fa fa-shopping-cart"></i></a>
+			</div>
+			<ul class="main-menu">
+				<li class="active"><a href="index.html">Home</a></li>
+				<li><a href="about.html">About Us</a></li>
+				
+				<li><a href="course.html">COURSES</a></li>
+				<li><a href="disclamer.html">Disclaimer</a></li>
+				<li><a href="contact.html">Contact</a></li>
+				<li><a href="privarcy.html">Privacy Policy
+				</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!-- Header section end -->
+</body>
+</html>
